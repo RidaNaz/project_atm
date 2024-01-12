c3a43cf1dd2d3bda0d8e84ae5e8359b6d5391bf6
RidaNaz <rnaz3414@gmail.com>
2024-01-12 12:14:09 +0500

    Initial commit

---
+++ atm.ts
@@ -0,0 +1,68 @@
+import inquirer from 'inquirer';
+
+interface UserData {
+    userId: string,
+    userPin: number,
+    accountType: string,
+    transactionType: string,
+    amount: number
+}
+
+async function atm() {
+    console.log ("Welcome to ABC Bank!")
+    const userData: UserData = await inquirer.prompt([
+        {
+            type: "string",
+            name: "userId",
+            message: "Insert your card and enter ID"
+        }, {
+            type: "number",
+            name: "userPin",
+            message: "Enter 4-digit pin"
+        }, {
+            type: "list",
+            name: "accountType",
+            message: "Select your account type",
+            choices: ["Current Account", "Saving Account"]
+        }, {
+            type: "list",
+            name: "transactionType",
+            message: "Select transaction type",
+            choices: ["Fast Cash Withdrawl", "Normal Withdrawl", "Balance"]
+        }, {
+            type: "list",
+            name: "amount",
+            message: "Select withdrawl amount",
+            choices: ["PKR 1000", "PKR 2000", "PKR 5000", "PKR 10000"],
+            when(userData) {
+                return userData.transactionType === "Fast Cash Withdrawl"
+            },
+        }, {
+            type: "number",
+            name: "amount",
+            message: "Enter withdrawl amount",
+            when(userData) {
+                return userData.transactionType === "Normal Withdrawl"
+            },
+        }
+    ])
+    
+        const balance = Math.floor(Math.random() * 1000000)
+
+        if (userData.transactionType === "Fast Cash Withdrawl" || userData.transactionType === "Normal Withdrawl") {
+            console.log(`Your current balance is PKR ${balance.toLocaleString()}`)
+
+            const remainingBal = balance - userData.amount
+
+            if (balance >= userData.amount) {
+
+                console.log(`Transaction successful! your remaining balance is PKR ${remainingBal.toLocaleString()}`)
+            } else {
+                console.log("Please try again with a lower amount")
+            }
+        } else if (userData.transactionType === "Balance") {
+            console.log(`Your current balance is PKR ${balance.toLocaleString()}`)
+        }
+}
+
+atm()
